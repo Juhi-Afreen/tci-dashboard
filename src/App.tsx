a3fb7ee39e62188f7df8fa17b16da361e4986c5c
Juhi Afreen <juhi@gmrwebteam.com>
2026-04-21 17:14:21 +0530

    fix: switch to HashRouter for GitHub Pages support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
